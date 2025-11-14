b7fc7246d910eb2e1bda637f783ba6c1a19c5829
anapremovic <apa109@sfu.ca>
2025-11-14 05:59:33 -0800

    Add util to normalize filepaths to prevent relative filepath issues

--- src/image_dataset.py
+++ src/image_dataset.py
@@ -1,18 +1,17 @@
-import os
 import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset
 import torch
+from util import get_path
 
 DIAGNOSIS_COLUMN_NAME = "diagnosis"
 POSITIVE_CLASS = "melanoma"
 IMAGE_FILEPATH_COLUMN_NAME = "derm"
 
 class MelanomaImageDataset(Dataset):
-
     def __init__(self, metadata_csv_path, images_dir, transform=None):
-        self.metadata_dataframe = pd.read_csv(metadata_csv_path)
-        self.images_directory = images_dir
+        self.metadata_dataframe = pd.read_csv(get_path(metadata_csv_path))
+        self.images_directory = get_path(images_dir)
         self.transform = transform
 
     def __len__(self):
@@ -21,7 +20,7 @@ class MelanomaImageDataset(Dataset):
     def __getitem__(self, index):
         metadata_row = self.metadata_dataframe.iloc[index]
         image_relative_path = metadata_row[IMAGE_FILEPATH_COLUMN_NAME]
-        image_absolute_path = os.path.join(self.images_directory, image_relative_path)
+        image_absolute_path = self.images_directory.joinpath(image_relative_path)
 
         image = Image.open(image_absolute_path).convert("RGB")
         label = 1 if POSITIVE_CLASS in metadata_row[DIAGNOSIS_COLUMN_NAME] else 0
