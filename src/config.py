4b63058defc3f44b389b18cf2bae8882844998b4
anapremovic <apa109@sfu.ca>
2025-11-14 06:36:51 -0800

    Move error-prone raw strings to config.py

---
+++ src/config.py
@@ -0,0 +1,13 @@
+from util import get_path
+
+# file paths
+IMAGE_DIRECTORY = get_path("data/image_data/images")
+METADATA_FILE = get_path("data/image_data/meta/meta.csv")
+TRAIN_INDICES_FILE = get_path("data/image_data/meta/train_indexes.csv")
+VALIDATION_INDICES_FILE = get_path("data/image_data/meta/valid_indexes.csv")
+TEST_INDICES_FILE = get_path("data/image_data/meta/test_indexes.csv")
+
+# meta.csv values
+DIAGNOSIS_COLUMN_NAME = "diagnosis"
+IMAGE_FILEPATH_COLUMN_NAME = "derm"
+POSITIVE_CLASS = "melanoma"
--- src/image_dataset.py
+++ src/image_dataset.py
@@ -4,10 +4,7 @@ from torch.utils.data import Dataset
 import torch
 from pathlib import Path
 from typing import Callable, Optional, Tuple, Union
-
-DIAGNOSIS_COLUMN_NAME = "diagnosis"
-POSITIVE_CLASS = "melanoma"
-IMAGE_FILEPATH_COLUMN_NAME = "derm"
+import config
 
 class MelanomaImageDataset(Dataset):
     def __init__(self, metadata_csv_path: Path, images_dir: Path, transform: Optional[Callable] = None):
@@ -20,11 +17,11 @@ class MelanomaImageDataset(Dataset):
 
     def __getitem__(self, index: int) -> Tuple[Union[Image.Image, torch.Tensor], torch.Tensor]:
         metadata_row = self.metadata_dataframe.iloc[index]
-        image_relative_path = metadata_row[IMAGE_FILEPATH_COLUMN_NAME]
+        image_relative_path = metadata_row[config.IMAGE_FILEPATH_COLUMN_NAME]
         image_absolute_path = self.images_directory.joinpath(image_relative_path)
 
         image = Image.open(image_absolute_path).convert("RGB")
-        label = 1 if POSITIVE_CLASS in metadata_row[DIAGNOSIS_COLUMN_NAME] else 0
+        label = 1 if config.POSITIVE_CLASS in metadata_row[config.DIAGNOSIS_COLUMN_NAME] else 0
 
         if self.transform:
             image = self.transform(image)
