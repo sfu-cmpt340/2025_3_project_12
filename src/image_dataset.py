89deedfe7545f8f7530955919fbd7460b2353916
anapremovic <apa109@sfu.ca>
2025-11-14 05:08:12 -0800

    Implement getting a data item and length

--- src/image_dataset.py
+++ src/image_dataset.py
@@ -3,29 +3,30 @@ import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset
 import torch
-from torchvision import transforms
 
 DIAGNOSIS_COLUMN_NAME = "diagnosis"
-CLASS_LABEL_COLUMN_NAME = "label"
 POSITIVE_CLASS = "melanoma"
+IMAGE_FILEPATH_COLUMN_NAME = "derm"
 
 class MelanomaImageDataset(Dataset):
 
     def __init__(self, metadata_csv_path, images_dir, transform=None):
         self.metadata_dataframe = pd.read_csv(metadata_csv_path)
-        self.images_dir = images_dir
+        self.images_directory = images_dir
         self.transform = transform
 
-        # normalize
-        self.metadata_dataframe[DIAGNOSIS_COLUMN_NAME] = (self.metadata_dataframe[DIAGNOSIS_COLUMN_NAME].fillna("").str.strip().str.lower())
-
-        # add class label
-        self.metadata_dataframe[CLASS_LABEL_COLUMN_NAME] = self.metadata_dataframe[DIAGNOSIS_COLUMN_NAME].apply(
-            lambda d: 1 if POSITIVE_CLASS in d else 0
-        )
-
     def __len__(self):
-        return 0
+        return len(self.metadata_dataframe)
 
     def __getitem__(self, index):
-        return None
+        metadata_row = self.metadata_dataframe.iloc[index]
+        image_relative_path = metadata_row[IMAGE_FILEPATH_COLUMN_NAME]
+        image_absolute_path = os.path.join(self.images_directory, image_relative_path)
+
+        image = Image.open(image_absolute_path).convert("RGB")
+        label = 1 if POSITIVE_CLASS in metadata_row[DIAGNOSIS_COLUMN_NAME] else 0
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, torch.tensor(label, dtype=torch.long)
