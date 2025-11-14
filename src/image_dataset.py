7950d7ce1fc79b61862a609eef3b89c3a8c581b7
anapremovic <apa109@sfu.ca>
2025-11-14 06:52:47 -0800

    Add documentation

--- src/image_dataset.py
+++ src/image_dataset.py
@@ -7,15 +7,35 @@ from typing import Callable, Optional, Tuple, Union
 import config
 
 class MelanomaImageDataset(Dataset):
+    """
+    PyTorch Dataset class to represent SFU dermascopic criteria melanoma image set.
+    Implements minimum required functions for Dataset interface.
+    """
+
     def __init__(self, metadata_csv_path: Path, images_dir: Path, transform: Optional[Callable] = None):
+        """
+        Initializes MelanomaImageDataset with metadata, image directories, and optional transform.
+        :param metadata_csv_path: Path object to CSV file containing image metadata.
+        :param images_dir: Path object to directory containing all images.
+        :param transform: Optional function to transform images to normalize for use with classifier model.
+        """
         self.metadata_dataframe = pd.read_csv(metadata_csv_path)
         self.images_directory = images_dir
         self.transform = transform
 
     def __len__(self) -> int:
+        """
+        Get size of dataset.
+        :return: Size.
+        """
         return len(self.metadata_dataframe)
 
     def __getitem__(self, index: int) -> Tuple[Union[Image.Image, torch.Tensor], torch.Tensor]:
+        """
+        Get one data element from the dataset.
+        :param index: Index of data element.
+        :return: The element's image and label (melanoma vs not melanoma).
+        """
         metadata_row = self.metadata_dataframe.iloc[index]
         image_relative_path = metadata_row[config.IMAGE_FILEPATH_COLUMN_NAME]
         image_absolute_path = self.images_directory.joinpath(image_relative_path)
