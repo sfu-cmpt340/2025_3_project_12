4b0349519504b9e2203601f386f4f1ba69654984
anapremovic <apa109@sfu.ca>
2025-11-14 06:52:47 -0800

    Add documentation

--- src/image_dataset_loader.py
+++ src/image_dataset_loader.py
@@ -12,6 +12,11 @@ class LoaderType(Enum):
     TEST = 3
 
 def get_melanoma_image_dataset_loader(loader_type: LoaderType) -> DataLoader:
+    """
+    Gets data loader for any phase of AI pipeline.
+    :param loader_type: The phase (TRAINING, VALIDATION, or TEST).
+    :return: The data loader.
+    """
     full_dataset = MelanomaImageDataset(
         metadata_csv_path = config.METADATA_FILE,
         images_dir= config.IMAGE_DIRECTORY,
@@ -30,6 +35,11 @@ def get_melanoma_image_dataset_loader(loader_type: LoaderType) -> DataLoader:
     return DataLoader(loader_type_subset, batch_size=32, shuffle=shuffle)
 
 def _load_indices(csv_path: Path) -> list:
+    """
+    Converts indices of images to use for phase of AI pipeline (as suggested by SFU dataset) to list.
+    :param csv_path: Path object of CSV file contain indices.
+    :return: CSV file as a list of ints.
+    """
     indices = []
     with open(csv_path) as file:
         reader = csv.reader(file)
@@ -39,6 +49,10 @@ def _load_indices(csv_path: Path) -> list:
     return indices
 
 def _get_inception_v3_image_transform() -> transforms.Compose:
+    """
+    Gets transformation for use of images in Inception V3 model. The model requires 299x299 images.
+    :return: A function performing the transformation.
+    """
     return transforms.Compose([
         transforms.Resize((299, 299)),
         transforms.ToTensor(),
--- src/model.py
+++ src/model.py
@@ -2,6 +2,11 @@ import torch.nn as nn
 import torchvision.models as models
 
 def load_inception_v3(num_classes=2):
+    """
+    Loads to Inception V3 model as a binary classifier by default.
+    :param num_classes: The number of classes if not default.
+    :return: The model.
+    """
     model = models.inception_v3(weights="IMAGENET1K_V1")
     model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
