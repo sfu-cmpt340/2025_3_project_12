2599e79d9ddad8c471ba0d2d660310a223936e53
anapremovic <apa109@sfu.ca>
2025-11-14 06:40:08 -0800

    Remove unnecessary class use functions instead

--- src/image_dataset_loader.py
+++ src/image_dataset_loader.py
@@ -11,41 +11,38 @@ class LoaderType(Enum):
     VALIDATION = 2
     TEST = 3
 
-class ImageDatasetLoader:
-    def get_loader(self, loader_type: LoaderType) -> DataLoader:
-        dataset = MelanomaImageDataset(
-            metadata_csv_path = config.METADATA_FILE,
-            images_dir= config.IMAGE_DIRECTORY,
-            transform=self._get_inception_v3_image_transform()
-        )
+def get_loader(loader_type: LoaderType) -> DataLoader:
+    dataset = MelanomaImageDataset(
+        metadata_csv_path = config.METADATA_FILE,
+        images_dir= config.IMAGE_DIRECTORY,
+        transform=_get_inception_v3_image_transform()
+    )
 
-        loader_map = {
-            LoaderType.TRAINING: (config.TRAIN_INDICES_FILE, True),
-            LoaderType.VALIDATION: (config.VALIDATION_INDICES_FILE, False),
-            LoaderType.TEST: (config.TEST_INDICES_FILE, False)
-        }
+    loader_map = {
+        LoaderType.TRAINING: (config.TRAIN_INDICES_FILE, True),
+        LoaderType.VALIDATION: (config.VALIDATION_INDICES_FILE, False),
+        LoaderType.TEST: (config.TEST_INDICES_FILE, False)
+    }
 
-        file_path, shuffle = loader_map[loader_type]
-        indices = self._load_indices(file_path)
-        subset_dataset = Subset(dataset, indices)
-        return DataLoader(subset_dataset, batch_size=32, shuffle=shuffle)
+    file_path, shuffle = loader_map[loader_type]
+    indices = _load_indices(file_path)
+    subset_dataset = Subset(dataset, indices)
+    return DataLoader(subset_dataset, batch_size=32, shuffle=shuffle)
 
-    @staticmethod
-    def _load_indices(csv_path: Path) -> list:
-        indices = []
-        with open(csv_path) as file:
-            reader = csv.reader(file)
-            for row in reader:
-                indices.append(int(row[0]))
+def _load_indices(csv_path: Path) -> list:
+    indices = []
+    with open(csv_path) as file:
+        reader = csv.reader(file)
+        for row in reader:
+            indices.append(int(row[0]))
 
-        return indices
+    return indices
 
-    @staticmethod
-    def _get_inception_v3_image_transform() -> transforms.Compose:
-        return transforms.Compose([
-            transforms.Resize((299, 299)),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]),
-        ])
+def _get_inception_v3_image_transform() -> transforms.Compose:
+    return transforms.Compose([
+        transforms.Resize((299, 299)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]),
+    ])
