57462aa8e053c8a4b36ac5ee20fb4a5b30ee4260
anapremovic <apa109@sfu.ca>
2025-11-14 20:23:34 -0800

    Train model on images

--- src/model.py
+++ src/model.py
@@ -1,7 +1,8 @@
 import torch.nn as nn
 import torchvision.models as models
+from torchvision.models import Inception3
 
-def load_inception_v3(num_classes=2):
+def load_inception_v3(num_classes=2) -> Inception3:
     """
     Loads to Inception V3 model as a binary classifier by default.
     :param num_classes: The number of classes if not default.
