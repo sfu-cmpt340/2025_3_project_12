53ba80df2b4556fc0aa564bc45598b24a4e289ca
temek999999 <233860706+temek999999@users.noreply.github.com>
2025-11-25 12:11:08 -0800

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,14 @@
+from util import get_path
+
+# file paths
+IMAGES_ZIP_PATH_1 = get_path("data/image_data/images1.zip")
+IMAGES_ZIP_PATH_2 = get_path("data/image_data/images2.zip")
+METADATA_FILE = get_path("data/image_data/meta/meta.csv")
+TRAIN_INDICES_FILE = get_path("data/image_data/meta/train_indexes.csv")
+VALIDATION_INDICES_FILE = get_path("data/image_data/meta/valid_indexes.csv")
+TEST_INDICES_FILE = get_path("data/image_data/meta/test_indexes.csv")
+
+# meta.csv values
+DIAGNOSIS_COLUMN_NAME = "diagnosis"
+IMAGE_FILEPATH_COLUMN_NAME = "derm"
+POSITIVE_CLASS = "melanoma"
