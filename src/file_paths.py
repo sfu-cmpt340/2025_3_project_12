74d315bb4c32667603b6b5ba7ec765b18f33a023
anapremovic <apa109@sfu.ca>
2025-11-29 15:42:56 -0800

    Rename zip files for clarity and update file_paths to not use unnecessary util

--- src/file_paths.py
+++ src/file_paths.py
@@ -1,14 +1,27 @@
-from .util import get_path
+import os
 
+from pathlib import Path
+
+def _get_data_directory() -> Path:
+    project_root = os.path.abspath(__file__).parent()
+    data_directory = project_root / "data"
+    return Path(os.path.dirname(data_directory))
+
+REAL_IMAGE_DIRECTORY = _get_data_directory() / "image_data"
+SKETCH_DIRECTORY = _get_data_directory() / "sketch_data"
 
 # real images
-IMAGES_ZIP_PATH_1 = get_path("data/image_data/images1.zip")
-IMAGES_ZIP_PATH_2 = get_path("data/image_data/images2.zip")
-METADATA_FILE = get_path("data/image_data/meta/meta.csv")
-TRAIN_INDICES_FILE = get_path("data/image_data/meta/train_indexes.csv")
-VALIDATION_INDICES_FILE = get_path("data/image_data/meta/valid_indexes.csv")
-TEST_INDICES_FILE = get_path("data/image_data/meta/test_indexes.csv")
+IMAGES_ZIP_PATH_1 = REAL_IMAGE_DIRECTORY / "images1.zip"
+IMAGES_ZIP_PATH_2 = REAL_IMAGE_DIRECTORY / "images2.zip"
+METADATA_FILE = REAL_IMAGE_DIRECTORY / "/meta/meta.csv"
+TRAIN_INDICES_FILE = REAL_IMAGE_DIRECTORY / "/meta/train_indexes.csv"
+VALIDATION_INDICES_FILE = REAL_IMAGE_DIRECTORY / "/meta/valid_indexes.csv"
+TEST_INDICES_FILE = REAL_IMAGE_DIRECTORY / "/meta/test_indexes.csv"
 
 # sketches
-SKETCH_SPLITS_ZIP = get_path("sketch_splits.zip")  
-SKETCH_DATA_ROOT  = get_path("sketch_splits") 
+MALIGNANT_SKETCHES_ZIP = SKETCH_DIRECTORY / "malignant_sketches.zip"
+BENIGN_SKETCHES_ZIP = SKETCH_DIRECTORY / "benign_sketches.zip"
+MALIGNANT_SKETCHES_AUGMENTED_OUTPUT = SKETCH_DIRECTORY / "malignant_sketches_augmented"
+BENIGN_SKETCHES_AUGMENTED_OUTPUT = SKETCH_DIRECTORY / "benign_sketches_augmented"
+SKETCH_SPLITS_ZIP = SKETCH_DIRECTORY / "sketch_splits.zip"
+SKETCH_SPLITS = SKETCH_DIRECTORY / "sketch_splits"
